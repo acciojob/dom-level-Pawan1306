ac9c6f1b5ca044e38be520dcce035011d7f1b592
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-11-02 22:30:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Get the element with the specified ID
+var element = document.getElementById("level");
+
+// Function to calculate the DOM level
+function getDomLevel(element) {
+    var level = 0;
+    while (element && element !== document) {
+        level++;
+        element = element.parentNode;
+    }
+    return level;
+}
+
+// Calculate the DOM level and display it using alert
+var domLevel = getDomLevel(element);
+alert("The level of the element is: " + domLevel);
